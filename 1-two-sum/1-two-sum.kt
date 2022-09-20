89563bb3153f8bbba6a6c977ec7e943ce1c962d6
Jaeyeon Park <donionbs7@gmail.com>
2022-09-20 16:02:45 +0900

    Time: 401 ms (54.94%), Space: 42 MB (52.55%) - LeetHub

--- 1-two-sum/1-two-sum.kt
+++ 1-two-sum/1-two-sum.kt
@@ -2,13 +2,10 @@ class Solution {
     fun twoSum(nums: IntArray, target: Int): IntArray {
         val toMap = HashMap<Int,Int>()
         for ((index,value) in nums.withIndex()){
-            toMap[value] = index
-        }
-        
-        nums.forEachIndexed { index, value ->
             if(target - value in toMap && index != toMap[target-value]){
                 return intArrayOf(index,toMap[target-value]!!)
             }
+            toMap[value] = index
         }
         return intArrayOf(0,0)
     }
